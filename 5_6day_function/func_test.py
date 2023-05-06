86280b1f8aa9f8058665634d411e1a3c7583e18a
HannahLinden <1148289395@qq.com>
2023-05-06 23:00:12 +0800

    这是关于函数function的代码

---
+++ 5_6day_function/func_test.py
@@ -0,0 +1,15 @@
+name="abcd"
+name2="abc"
+# count=0
+# for i in name:
+#     count+=1
+# print(count)
+
+def my_len(date):
+    count=0
+    for i in date:
+        count += 1
+    print(count)
+
+my_len(name)
+my_len(name2)
---
+++ 5_6day_function/func_test2.py
@@ -0,0 +1,8 @@
+def hi():
+    print("!11")
+    print(("222"))
+
+def add(num1,num2):
+    return num1+num2
+hi()
+print(f"1+2={add(1,2)}")
